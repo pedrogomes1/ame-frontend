2fe231c7f5a7e2a10fac644209a83c3aee7538bc
Pedro <pedro-gomes17@hotmail.com>
2022-05-27 08:32:36 -0300

    chore: added onChange function and some id, htmlFor and value attributes in inputs

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -19,39 +19,71 @@ export function ContactForm(props) {
     onSubmit(contact);
   }
 
-  function fieldChange(event) {
+  function handleChangeField(event) {
+    const fieldName = event.target.name;
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
-  }
-
-  function isSelected(key, option) {
-    // return this.props.data[key] == option;
+    onChange({ ...contact, [fieldName]: value });
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>Contact Form</h3>
 
       <div className="form-group">
-        <label className="form-label">Your Name:</label>
-        <input name="name" className="form-control" />
+        <label className="form-label" htmlFor="fname">
+          Your Name:
+        </label>
+        <input
+          name="name"
+          className="form-control"
+          id="fname"
+          onChange={handleChangeField}
+          value={name}
+        />
       </div>
 
       <div className="form-group">
-        <label className="form-label">Your Best Email:</label>
-        <input name="email" className="form-control" />
+        <label className="form-label" htmlFor="femail">
+          Your Best Email:
+        </label>
+        <input
+          name="email"
+          className="form-control"
+          id="femail"
+          onChange={handleChangeField}
+          value={email}
+        />
       </div>
 
       <label className="form-label">Select your membership option:</label>
       <div className="form-group row">
         <label className="form-label col-xs-4">
-          <input type="radio" name="option" value="A" /> Option A
+          <input
+            type="radio"
+            name="option"
+            value="A"
+            onChange={handleChangeField}
+          />{" "}
+          Option A
         </label>
         <label className="form-label col-xs-4">
-          <input type="radio" name="option" value="B" /> Option B
+          <input
+            type="radio"
+            name="option"
+            value="B"
+            onChange={handleChangeField}
+          />{" "}
+          Option B
         </label>
         <label className="form-label col-xs-4">
-          <input type="radio" name="option" value="C" /> Option C
+          <input
+            type="radio"
+            name="option"
+            value="C"
+            onChange={handleChangeField}
+          />{" "}
+          Option C
         </label>
       </div>
 
@@ -59,25 +91,37 @@ export function ContactForm(props) {
 
       <div className="form-group">
         <label className="form-label">What can we help you with:</label>
-        <select className="form-control" name="select">
-          <option value="1">I have question about my membership</option>
+        <select
+          className="form-control"
+          name="select"
+          onChange={handleChangeField}
+        >
+          {options.map((option) => (
+            <option key={option.id} value={option.id}>
+              {option.label}
+            </option>
+          ))}
         </select>
       </div>
 
       <div className="form-group">
-        <label className="form-label">Message:</label>
+        <label className="form-label" htmlFor="fmessage">
+          Message:
+        </label>
         <textarea
           name="message"
           rows="10"
+          id="fmessage"
           placeholder="Please type your question here"
           className="form-control"
+          onChange={handleChangeField}
         />
       </div>
 
       <div className="form-group">
         <label className="form-label">
-          {" "}
-          <input type="checkbox" name="terms" /> I agree to terms and conditions{" "}
+          <input type="checkbox" name="terms" onChange={handleChangeField} /> I
+          agree to terms and conditions
         </label>
       </div>
 
